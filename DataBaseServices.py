12f6a430ce6e99cda1c2df9d259f0151afc99f2b
Enmanuel Batista <enmanuelbcf@gmail.com>
2026-01-05 10:56:12 -0400

    feature/Creacion de los servicios de Database

---
+++ DataBaseServices.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+
+class DataBaseServices:
+    def __init__(self):
+        self.connection = sqlite3.connect("database.db")
+
+    def create_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                usuario TEXT NOT NULL,
+                contrasena TEXT NOT NULL
+            )
+            """
+        )
+        self.connection.commit()
+        cursor.close()
+
+    def execute_query(self, query, params=()):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query, params)
+            self.connection.commit()
+            cursor.close()
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+
+    def fetch_query(self, query, params=()):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query, params)
+            results = cursor.fetchall()
+            cursor.close()
+            return results
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+            return None
+        
+    
+    def fetch_one(self, query, params=()):
+      try:
+          cursor = self.connection.cursor()
+          cursor.execute(query, params)
+          result = cursor.fetchone()
+          cursor.close()
+          return result
+      except sqlite3.Error as e:
+          print(f"An error occurred: {e}")
+          return None
+            
